fc1bc4f1dc1f63eff5055ce9b3c41180bf9f9883
cherotwokibet <bkcherotwo@gmail.com>
2021-10-27 14:48:45 +0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {AppLoading} from 'expo';
+import {firebaseConfig} from './app/api/fbConfig';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+import navigationTheme from './app/navigation/navigationTheme';
+import AppNavigator from './app/navigation/AppNavigator';
+import OfflineNotice from './app/components/OfflineNotice';
+import AuthNavigator from './app/navigation/AuthNavigator';
+import AuthContext from './app/auth/context';
+import { navigationRef } from './app/navigation/rootNavigation';
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}else {
+  firebase.app(); // if already initialized, use that one
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default function App() {
+  console.log('start');
+  const [user,setUser] = useState();
+  const [isReady,setIsReady] = useState(false);
+
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    // const user = firebase.auth().currentUser;
+    if(user) setUser(user);
+  };
+
+  if(isReady){
+    return (
+      <AppLoading
+        startAsync={restoreUser}
+        onFinish={()=> setIsReady(true)}
+      />
+    );
+  }
+
+
+  return(
+    <AuthContext.Provider value={{user,setUser}}>
+      <OfflineNotice/>
+      <NavigationContainer ref={navigationRef} theme={navigationTheme}>
+        {user ? <AppNavigator/> : <AuthNavigator/> }
+      </NavigationContainer>
+    </AuthContext.Provider>
+  );
+
+}
